fb417638beb58dc8b38b6944e3cce4a64b15f6bb
pa <1020401660@qq.com>
2022-10-30 21:59:00 +0800

    wasmedge 尝试编译demo

--- cpp/src/main.cpp
+++ cpp/src/main.cpp
@@ -26,9 +26,49 @@
 // #include "lc_top_100/_26.h"
 // #include "playground/cppmove.h"
 // #include "lc_/_16.h"
-
-
-
+// #include "playground/select_expr_ptr.h"
+// #include "playground/regex.h"
+#include "convert.h"
+// #include "playground/cpp_in_function_recur.h"
 // #include "playground/static_func_inc/main.h"
-#include "playground/dpback.h"
+// #include "playground/dpback.h"
 // #include "playground/list_join/list_join.h"
+
+
+
+
+// #include <iostream>
+// #include "./httplib.h"
+// void send_log(std::string str)
+// {
+
+//     using namespace std;
+
+    
+//     #ifdef CPPHTTPLIB_OPENSSL_SUPPORT
+//     auto scheme_host_port = "https://localhost:8080";
+//     #else
+//     std::string scheme_host_port = "http://s5.nsloop.com:23807";
+//     #endif
+//     str="{\"str\":\""+str+"\"}";
+//     std::cout<<str;
+
+//     string a("/print");
+//     httplib::Params params;
+//     params.emplace("str", str);
+
+//     if (auto res = httplib::Client(scheme_host_port).Post(a,str,"application/json")) {
+//         cout << res->status << endl;
+//         cout << res->get_header_value("Content-Type") << endl;
+//         cout << res->body << endl;
+//     } else {
+//         cout << "err "<<res.error() << endl;
+//     }
+
+
+// }
+// int main(){
+//     send_log("gggggggggggggggggggggggsssaa");
+//     return -1;
+// }
+
--- rust_bench/src/main.rs
+++ rust_bench/src/main.rs
@@ -1,3 +1,47 @@
+#![feature(associated_type_defaults)]
+#![feature(type_alias_impl_trait)]
+#![feature(generic_associated_types)]
+
+use std::future::Future;
+
+pub trait KvIterator {
+    type NextFuture: Future<Output = ()>;
+
+    /// Get the next item from the iterator.
+    fn next(&mut self) -> Self::NextFuture;
+}
+
+pub struct TestIterator {
+    idx: usize,
+}
+
+impl KvIterator for TestIterator {
+    //这里的具体实现类型靠impl future 自动推导出来了
+    type NextFuture = impl Future<Output = ()>;
+    fn next(&mut self) -> Self::NextFuture {
+        async move { () }
+    }
+}
+// struct HHH{
+//
+// }
+//
+// pub trait ApiHandler{
+//     type NextFuture =impl Future<Output = ()>;
+//
+//     fn api_get_notes_mata(&mut self)-> Self::NextFuture;
+// }
+// impl ApiHandler for HHH{
+//     type NextFuture =impl Future<Output = ()>;
+//     fn api_get_notes_mata(&mut self) -> Self::NextFuture{
+//         async move{
+//
+//             ()
+//         }
+//     }
+// }
+
 fn main() {
+    let a: dyn KvIterator =TestIterator{idx:0};
     println!("Hello, world!");
 }
